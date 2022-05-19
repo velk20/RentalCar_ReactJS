bce57606143ff00211553523250a30a6481df903
Angel Mladenov <48069264+velk20@users.noreply.github.com>
2022-05-19 18:52:13 +0300

    axios and bootstrap

--- src/components/vehicles/vehicle-card/VehicleCard.js
+++ src/components/vehicles/vehicle-card/VehicleCard.js
@@ -1,16 +1,37 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import './VehicleCard.scss';
+
 export function VehicleCard({ user }) {
   return (
-    <div className="vehicle-card">
-      <div className="card-header">
-        <h4>Angel Mladenov</h4>
-      </div>
-      <div className="card-body">
-        <img src="dsa.png" alt="vehicle-image" />
-      </div>
-      <div className="card-footer">
-        <button className="edit-btn">Edit</button>
-        <button className="delete-btn">Delete</button>
-      </div>
-    </div>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img
+        variant="top"
+        src="https://img.autoabc.lv/Lexus-ES/Lexus-ES_2006_Sedans_1641210554_3.jpg"
+      />
+      <Card.Body>
+        <Card.Title>Vehicle Brand and Model</Card.Title>
+        <Card.Text>
+          <span className="key">Year: </span>
+          <span className="value">2012</span>
+        </Card.Text>
+        <Card.Text>
+          <span className="key">Type: </span>
+          <span className="value">Van</span>
+        </Card.Text>
+        <Card.Text>
+          <span className="key">Fuel: </span>
+          <span className="value">Petrol</span>
+        </Card.Text>
+        <Card.Text>
+          <span className="key">Number of seats: </span>
+          <span className="value">5</span>
+        </Card.Text>
+        <div className="btn-holder">
+          <Button variant="primary">Edit</Button>
+          <Button variant="danger">Delete</Button>
+        </div>
+      </Card.Body>
+    </Card>
   );
 }
